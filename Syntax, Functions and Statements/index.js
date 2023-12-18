98770f1b0f4a1c3628b71e119a78ff57bba72d88
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-12-18 14:17:48 +0200

    functio decl, expr and arrow

---
+++ Syntax, Functions and Statements/index.js
@@ -0,0 +1,14 @@
+// Function Declaration
+function walk() {
+    console.log("walking");
+}
+
+// Function Expression
+const walk = function () {
+    console.log("walking");
+};
+
+// Arrow Function
+const walk = () => {
+    console.log("walking");
+};
