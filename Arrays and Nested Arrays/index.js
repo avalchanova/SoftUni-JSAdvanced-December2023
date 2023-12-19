e14c71f98f0b969a4cd88aec7f5b437106628b7e
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-12-19 16:28:52 +0200

    Arrays Indexation

--- Arrays and Nested Arrays/index.js
+++ Arrays and Nested Arrays/index.js
@@ -5,3 +5,20 @@ mixedArr[4] = () => console.log('I am the fourth element');
 
 mixedArr[4](); // cool thing here
 // evoking the function in the fourth element
+
+
+
+// Arrays Indexation
+
+// Setting values via non-integers using bracket notation (or dot notation) creates
+// object properties instead of array elements(will be discussed in later lesson);
+
+let arr = [];
+arr[3.4] = 'Oranges';
+arr[-1] = 'Apples';
+console.log(arr.length);
+console.log(arr.hasOwnProperty(3.4));
+arr["1"] = 'Grapes';
+console.log(arr.length); // 2
+console.log(arr); //  […, 'Grapes', …, 'Oranges', 'Apples', …]
+// this was mind fucking
