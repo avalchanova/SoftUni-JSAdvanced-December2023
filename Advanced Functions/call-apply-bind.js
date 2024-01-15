05c616cd6f097cde40841a59d67bd12db2291a83
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2024-01-15 17:34:52 +0200

    call method + apply method

--- Advanced Functions/call-apply-bind.js
+++ Advanced Functions/call-apply-bind.js
@@ -10,3 +10,10 @@ let person = {
     name: 'Pesho'
 };
 
+introduce.call(person, "Gosho", "Pesho"); // invoke using call()
+// call is a method for the function
+// so call uses the context of person to envoke the introduce function
+// we will use parameters by spreading them like that when it is call() method
+
+introduce.apply(person, ["Gosho", "Pesho"]);
+// parameters for apply() are used in []
