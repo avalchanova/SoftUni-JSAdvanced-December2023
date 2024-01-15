7dff9aea04a5b6b38e92db6233ddfa616ac2bae4
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2024-01-15 17:56:35 +0200

    bind()

--- Advanced Functions/call-apply-bind.js
+++ Advanced Functions/call-apply-bind.js
@@ -15,10 +15,20 @@ introduce.call(person, "Gosho", "Pesho"); // invoke using call()
 // so call uses the context of person to envoke the introduce function
 // we will use parameters by spreading them like that when it is call() method
 
-introduce.apply(person, ["Gosho", "Pesho"]);
+introduce.apply(person, ["Gosho", "Peshanov"]);
 // parameters for apply() are used in []
 // Apply uses Array - this way i can remember
 
 
 // реално тези методи се използват, за да ни позволяват да си извикаме функцията, като ни позволява
 // да имаме контрол и върху контекста, в който ще бъде извикана, и върху входните данни, които ще ѝ подоваме
+
+
+// Bind
+
+let superHuman = {
+    name: 'Superman'
+};
+
+let superIntroduction = introduce.bind(superHuman, 'Louis', 'Lane');
+superIntroduction();
