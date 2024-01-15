95e44cd5a10e8e2f14b5550eb019651b9637bc27
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2024-01-15 17:19:41 +0200

    this in an object: function declaration vs arrow function

--- Advanced Functions/index.js
+++ Advanced Functions/index.js
@@ -14,3 +14,19 @@ person.introduce();
 
 // this will return Hello, my name is undefined undefined
 // because the context of getFullName() is global therefore this points to the undefined
+
+// However, if we switch the function declaration and we make it function expression, the problem will  be solved:
+
+const person2 = {
+    name: "Pesho",
+    lastName: "Georgiev",
+    introduce() {
+        const getFullName = () => {
+            return this.name + ' ' + this.lastName;
+        };
+
+        console.log(`Hello, my name is ${getFullName()}`);
+    }
+};
+
+person2.introduce();
