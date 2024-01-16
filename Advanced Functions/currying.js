5bb4c303df64131931098772130dab1d6ba6858d
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2024-01-16 15:32:03 +0200

    currying: master of the arrow functions

--- Advanced Functions/currying.js
+++ Advanced Functions/currying.js
@@ -16,3 +16,8 @@ console.log(sum3(5)(6)(8));
 
 const arrowSum = a => b => c => a + b + c;
 console.log(arrowSum(5)(6)(8));
+
+let sum5 = sum3(5);
+let sum5and6 = sum5(6);
+let totalSum = sum5and6(8);
+console.log(totalSum);
