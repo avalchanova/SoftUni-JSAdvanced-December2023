880a5cdf9334efc09ee82c7e21bf8854bbbf4db2
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2024-01-16 15:22:17 +0200

    add exercise uses the bind method to bind the func with a null contextor this context which will force it to use the same context as the current context and then also another number as an argIIFE - immidiately invoked function expression

--- Advanced Functions/add.js
+++ Advanced Functions/add.js
@@ -5,6 +5,8 @@ function solution(number) {
 
     return add.bind(null, number);
     //uses the bind method to bind the func with a null contextor this context which will force it to use the same context as the current context and then also another number as an arg
+
+    // return (n) => add(number, n); --- this is the same as the bind solution
 }
 
 let add5 = solution(5);
