21da27647a35e2e8e0c7883d84d97c5f8fd7a666
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2024-01-16 09:49:10 +0200

    first-class-function

--- Advanced Functions/first-class-functions.js
+++ Advanced Functions/first-class-functions.js
@@ -1,5 +1,7 @@
 // Functions which was passed as an argument to another function
 // an example is addEventListener('givenEvent', handlerFunctionWhichIsActuallyFirstClassFunc)
+// each function in JS is a first class function - this is law!
+// this is because every functio can be passed as an argument, can be returned as a result and can be used as a variable
 
 function sayHello() {
     return function Hello() {
