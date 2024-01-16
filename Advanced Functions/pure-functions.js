47e3c339fb8499a4bdb31a54401d27533d7af280
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2024-01-16 14:05:32 +0200

    pure functions

---
+++ Advanced Functions/pure-functions.js
@@ -0,0 +1,6 @@
+// Pure functions do not have side effects
+// meaning: it will always return the same result given the same parameters
+// execution is independent of the state of the system
+// good examples are mathematical solutions
+
+// Math.random is an IMpure function
